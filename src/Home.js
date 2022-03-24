fae17427a5336178edf1713f6eec0a423d50192f
nholuong <luongutnho.dxc@gmail.com>
2022-03-24 18:48:25 +0700

    update text for check build

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Home extends Component {
     render() {
-        return <div>Home page Edit</div>
+        return <div>Home page Edit version 2</div>
     }
 }
 
