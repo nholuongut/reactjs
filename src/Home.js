a6dc36b628d4e4f2ee100ff55e6a0c8af2a24382
Nho Luong <58627821+nholuongut@users.noreply.github.com>
2022-03-31 16:26:30 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Home extends Component {
     render() {
-        return <div>Home page version demo done v1</div>
+        return <div>Home page version demo done v2</div>
     }
 }
 
