6763b971423ad02960b3a4e3804a31622b69d0ac
Nho Luong <58627821+nholuongut@users.noreply.github.com>
2022-03-31 16:05:54 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = 'http://lavareldemo-env.eba-2hitjwix.us-east-1.elasticbeanstalk.com/api';
 export const TRIVIA_ENDPOINT = 'http://jservice.io/api/random?count=1';
